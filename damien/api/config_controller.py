1131bfd03d18277a032dc28cf41ca22d7cb94b90
Anne Lyttle <alyttle@berkeley.edu>
2024-12-09 15:51:47 -0800

    DAMIEN-715: restores eval table row colors

--- damien/api/config_controller.py
+++ damien/api/config_controller.py
@@ -40,7 +40,7 @@ from flask_login import current_user, login_required
 def app_config():
     def _term_feed(term_id, default_meeting_dates, valid_meeting_dates):
         if not (default_meeting_dates and valid_meeting_dates):
-            app.logger.warn(f'No meeting dates found for term_id {term_id}')
+            app.logger.warning(f'No meeting dates found for term_id {term_id}')
             return {
                 'id': term_id,
                 'name': term_name_for_sis_id(term_id),
